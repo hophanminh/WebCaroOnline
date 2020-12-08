eaf353fef1c60bdd1fb5c5fb8512023fa9d08285
masa23vn <48360432+masa23vn@users.noreply.github.com>
2020-12-08 09:25:16 +0700

    Revert "up host"
    This reverts commit 5e205ad8e77070c8328055ef5b7e0e7cef9c0271.

--- Users/client/src/utils/socket.service.js
+++ Users/client/src/utils/socket.service.js
@@ -1,8 +1,8 @@
 import socketIOClient from "socket.io-client";
-import HostManagement from "./host.service"
 
 // test
-const API_URL = HostManagement.getHost;
+const API_URL = "http://localhost:9000/";
+//const API_URL = "https://midterm596server.herokuapp.com/";
 
 // connect to server
 const socket = socketIOClient(API_URL);
