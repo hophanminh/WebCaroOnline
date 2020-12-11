624307e718eac2104a56f7d1f5722fd307dcee10
masa23vn <48360432+masa23vn@users.noreply.github.com>
2020-12-11 22:08:54 +0700

    Change back to local

--- Users/client/src/utils/host.service.js
+++ Users/client/src/utils/host.service.js
@@ -1,6 +1,6 @@
 const getHostURL = () => {
-    //const API_URL = "http://localhost:9000/";
-    const API_URL = "https://caroonline-user-api.herokuapp.com/";
+    const API_URL = "http://localhost:9000/";
+    //const API_URL = "https://caroonline-user-api.herokuapp.com/";
     return API_URL;
   }
   
